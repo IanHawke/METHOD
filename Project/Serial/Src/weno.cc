bff0639e9404c40e9de36e8489a1a62c3832d9f7
Ian Hawke <I.Hawke@soton.ac.uk>
2020-05-26 13:57:14 +0100

    WENO5

--- Project/Serial/Src/weno.cc
+++ Project/Serial/Src/weno.cc
@@ -32,3 +32,44 @@ double weno3_upwind(double vec0, double vec1, double vec2)
 
   return (w0*stencils0 + w1*stencils1);
 }
+
+// This taken from the Shu review
+double weno5_upwind(double vec0, double vec1, double vec2, double vec3, double vec4)
+{
+
+
+  // Initialize the working arrays
+  double alpha0=0, alpha1=0, alpha2=0;
+  double beta0=0, beta1=0, beta2=0;
+  double stencils0=0, stencils1=0, stencils2=0;
+  double w0=0, w1=0, w2=0;
+  double epsilon = 1e-16;
+  double alphaSum = 0;
+
+  // Loopless weno5 upwind reconstruction of vec[]
+  beta0 = 13.0*pow(vec2-2.0*vec3+vec4, 2)/12.0
+  beta0 += pow(vec2-4.0*vec3+vec4, 2)/4.0
+  beta1 = 13.0*pow(vec1-2.0*vec2+vec3, 2)/12.0
+  beta1 += pow(v1-v3, 2)/4.0
+  beta2 = 13.0*pow(vec0-2.0*vec1+vec2, 2)/12.0
+  beta2 += pow(vec0-4.0*vec1+3.0*vec2, 2)/4.0
+  alpha0 = 0.3 / (epsilon + beta0 * beta0);
+  alpha1 = 0.6 / (epsilon + beta1 * beta1);
+  alpha2 = 0.1 / (epsilon + beta2 * beta2);
+  alphaSum = alpha0 + alpha1 + alpha2;
+  w0 = alpha0 / alphaSum;
+  w1 = alpha1 / alphaSum;
+  w2 = alpha2 / alphaSum;
+  // q^r = sum_{j=0}^{k-1} q_{i-r+j}
+  stencils0 += 1.0/3.0*vec2;
+  stencils0 += 5.0/6.0*vec3;
+  stencils0 += -1.0/6.0*vec4;
+  stencils1 += -1.0/6.0*vec1;
+  stencils1 += 5.0/6.0*vec2;
+  stencils1 += 1.0/3.0*vec3;
+  stencils2 += 1.0/3.0*vec0;
+  stencils2 += -7.0/6.0*vec1;
+  stencils2 += 11.0/6.0*vec2;
+
+  return (w0*stencils0 + w1*stencils1 + w2*stencils2);
+}
